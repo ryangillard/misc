590fdcc59fb3fb809fd7de14b8bfcff11c8ed5a1
Ryan Gillard <34553410+ryangillard@users.noreply.github.com>
2021-11-15 20:33:15 -0800

    simplified

--- leetcode/189_rotate_array/189_rotate_array.py
+++ leetcode/189_rotate_array/189_rotate_array.py
@@ -6,9 +6,4 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
-        nums_copy = nums[:]
-        nums_copy = nums_copy[-k:] + nums_copy[:-k]
-        for i in range(len(nums)):
-            nums[i] = nums_copy[i]
-
-        return None
+        nums[:] = nums[-k:] + nums[:-k]
